d2c520bd389df39b9f259b4419c1c590802828a6
Klobodanovic Adis <klobodanovic.adis.21@size.ba>
2024-03-18 01:08:37 +0100

    seed script

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-
-import { Button } from "@/components/ui/button";
 import { InfinityIcon } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
+import { courses } from "@/db/schema";
+
 type Props = {
-  activeCourse: { imageSrc: string; title: string }; // TODO: Replace with DB types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
