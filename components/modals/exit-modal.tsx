fa2df7eceb1c4544950479333ad2aed3dadb6083
Klobodanovic Adis <klobodanovic.adis.21@size.ba>
2024-03-20 00:47:34 +0100

    lesson header and exit modal

---
+++ components/modals/exit-modal.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useExitModal } from "@/store/use-exit-modal";
+
+export const ExitModal = () => {
+  const router = useRouter();
+  const [isclient, setIsClient] = useState(false);
+  const { isOpen, close } = useExitModal();
+
+  useEffect(() => setIsClient(true), []);
+
+  if (!isclient) return null;
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="flex items-center justify-center w-full mb-5">
+            <Image
+              src="/mascot_sad.svg"
+              alt="Sad mascot"
+              width={80}
+              height={80}
+            />
+          </div>
+          <DialogTitle className="text-center font-bold text-2xl">
+            Wait, don&apos;t go!
+          </DialogTitle>
+          <DialogDescription className="text-center">
+            You&apos;re about to leave the lesson. Are you sure?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="mb-4">
+          <div className="flex flex-col gap-y-4 w-full">
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              Keep learning
+            </Button>
+            <Button
+              variant="dangerOutline"
+              className="w-full"
+              size="lg"
+              onClick={() => {
+                close();
+                router.push("/learn");
+              }}
+            >
+              End session
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
